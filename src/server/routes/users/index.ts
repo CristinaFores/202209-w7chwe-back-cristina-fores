d53c3d62b7300f599fbdc911407b9732d4f7f29d
Cristina Fores <cristinaforescampos1992@gmail.com>
2022-11-12 14:53:42 +0100

    Add routers resgiterUsers

--- src/server/routes/users/index.ts
+++ src/server/routes/users/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser } from "../../controllers/users/index.js";
+import { registerUser } from "../../controllers/users/users.js";
 
 // eslint-disable-next-line new-cap
 const usersRouter = express.Router();
