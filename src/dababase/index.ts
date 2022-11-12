4088b84162680a88af915ed357ecfdd35cb256eb
Cristina Fores <cristinaforescampos1992@gmail.com>
2022-11-12 01:00:01 +0100

    Add tranform id data base in fuction connectDb

--- src/dababase/index.ts
+++ src/dababase/index.ts
@@ -3,4 +3,16 @@ import "../loadEnvironment.js";
 
 export const connectDb = async (mongoUrl: string) => {
   await mongoose.connect(mongoUrl);
+
+  mongoose.set("toJSON", {
+    virtuals: true,
+    transform(doc, ret) {
+      delete ret._id;
+      delete ret.__v;
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return ret;
+    },
+  });
 };
+
+export default connectDb;
