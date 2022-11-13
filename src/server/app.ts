a3f30c16a6cd6bcec3b6d8c9120a3ed84a4f28cb
Cristina Fores <cristinaforescampos1992@gmail.com>
2022-11-13 19:34:18 +0100

    Add cors in app

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,8 +3,13 @@ import express from "express";
 import morgan from "morgan";
 import usersRouter from "./routes/users/index.js";
 import { generalError, notFoundError } from "./middlewares/errors.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+
+app.disable("x-powered-by");
 
 app.use(morgan("dev"));
 app.use(express.json());
