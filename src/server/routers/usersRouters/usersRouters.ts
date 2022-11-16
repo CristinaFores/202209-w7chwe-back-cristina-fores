735d5149c2c8329c4445cd9cd3c6d116ab2dfe3b
Cristina Fores <cristinaforescampos1992@gmail.com>
2022-11-16 19:22:12 +0100

    Add router enpoint loadUsers

--- src/server/routers/usersRouters/usersRouters.ts
+++ src/server/routers/usersRouters/usersRouters.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { loginUser, registerUser } from "../../controllers/users/users.js";
+import {
+  loasUsers,
+  loginUser,
+  registerUser,
+} from "../../controllers/users/users.js";
 
 // eslint-disable-next-line new-cap
 const usersRouter = express.Router();
 
 usersRouter.post("/register", registerUser);
 usersRouter.post("/login", loginUser);
+usersRouter.get("/", loasUsers);
 export default usersRouter;
